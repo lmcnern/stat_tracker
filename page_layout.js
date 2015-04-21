b92c5ab48373b4ae97fc6ef9fe4d7a496bf90d45
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-21 16:09:03 -0700

    add smooth scrolling

--- page_layout.js
+++ page_layout.js
@@ -1,14 +1,41 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+var links = document.querySelectorAll(".content-links");
+var wrapper = document.querySelector("#content-wrap");
+
+// activeLink displays current selection
+var activeLink = 0;
+
+// Event Listeners
+for (var i = 0; i < links.length; i++) {
+  var link = links[i];
+  link.addEventListener('click', setClickedItem, false);
+
+  // Idefentifies the item for activeLink
+  link.itemID = i;
+}
+
+// set first item as active
+links[activeLink].classList.add("active");
+
+function setClickedItem(e) {
+  removeActiveLinks();
+
+  var clickedLink = e.target;
+  activeLink = clickedLink.itemID;
+
+  changePosition(clickedLink);
+}
+
+function removeActiveLinks() {
+  for (var i = 0; i < links.length; i++) {
+      links[i].classList.remove("active");
+  }
+}
+
+// Handle changing the slider position and ensures that
+// the correct link is highlighted as being active
+function changePosition(link) {
+  var position = link.getAttribute("data-pos");
+  wrapper.style.left = position;
+
+  link.classList.add("active");
+}
