c1024f76b075bec717084daa485e6ad44e470de9
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-22 17:58:27 -0700

    working chart

--- canvas.js
+++ canvas.js
@@ -1,28 +1,39 @@
 'use strict'
 
-// var e = document.getElementById('selectMenu');
-// var strUser = e.options[e.selectedIndex].value;
-
-// console.log(strUser);
-
-var StatData = function(label, data) {
-  this.label = label;
-  this.data = data;
+function jsFunction(value) {
+    if (value === 'PER') {
+      machine('PER');
+    } else if (value === 'TSP') {
+      machine('TSP');
+    } else if (value === '3PA') {
+      machine('3PA');
+    } else if (value === 'FTAR') {
+      machine('FTAR');
+    } else if (value === 'OWS') {
+      machine('OWS');
+    } else if (value === 'DWS') {
+      machine('DWS');
+    } else if (value === 'TWS') {
+      machine('TWS');
+    } else if (value === 'BPM') {
+      machine('BPM');
+    } else if (value === 'VORP') {
+      machine('VORP');
+    }
 }
 
-var FTr = new StatData(FTr, FTr);
+function machine(input) {
+  var barData = {
+    labels: myLabel(sorter(input)),
+    datasets: [
+      {
+        fillColor: "#66FF66",
+        strokeColor: "#006600",
+        data: myData(sorter(input))
+      },
+    ]
+  }
 
-
-var barData = {
-  labels: myLabel(sorter(FTr.label)),
-  datasets: [
-    {
-      fillColor: "#66FF66",
-      strokeColor: "#006600",
-      data: myData(sorter(FTr.data))
-    },
-  ]
+  var playerChart = document.getElementById("playerChart").getContext("2d");
+  new Chart(playerChart).Bar(barData);
 }
-
-var playerChart = document.getElementById("playerChart").getContext("2d");
-new Chart(playerChart).Bar(barData);
