752c4feee26803760d6a4f79b99c0c04a7c3a3cf
Mike Skalandunas <mskalandunas@gmail.com>
2015-04-22 17:09:59 -0700

    create constructor

--- canvas.js
+++ canvas.js
@@ -1,26 +1,28 @@
 'use strict'
 
-// var e = document.getElementById('selectMenu');
-// var strUser = e.options[e.selectedIndex].value;
+var e = document.getElementById('selectMenu');
+var strUser = e.options[e.selectedIndex].value;
+
+console.log(strUser);
 
 
 
-// console.log(strUser);
+var StatData = function(label, data) {
+  this.label = label;
+  this.data = data;
+}
+
+var FTr = new StatData(FTr, FTr);
+
 
 var barData = {
-  labels: myLabel(sorter('FTr')),
+  labels: myLabel(sorter(FTr.label)),
   datasets: [
     {
       fillColor: "#66FF66",
       strokeColor: "#006600",
-      data: myData(sorter('FTr'))
+      data: myData(sorter(FTr.data))
     },
-    // {
-    //   fillColor : "rgba(73,188,170,0.4)",
-    //   strokeColor : "rgba(72,174,209,0.4)",
-    //   data : [364,504,605,400,345,320]
-    // }
-
   ]
 }
 
