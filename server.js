ba834bc9f8bdd33d91aacb7e85ff9747fbde44f2
‘Justin <jsrdeluna@gmail.com>
2015-04-23 16:34:13 -0700

    Update server

--- server.js
+++ server.js
@@ -3,14 +3,11 @@
 var express = require('express'); //grabs from package.JSON
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
   response.status(200).send('<h1>Hello Stranger!</h1>');
-});
-
-app.get('/public', function(request, response) {
-  response.send('<h1>Hello mate!</h1>');
 });
 
 app.get('/secret', function(request, response) {
@@ -21,6 +18,6 @@ app.get('/*', function(request, response) {
   response.status(404).sendFile(__dirname +'/404.html');
 });
 
-app.listen(5000, function() {
-  console.log('Server is running!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
